91fba0b31a7100776257e627dd91f13a5bb14c79
unghoangnam <unghoangnam389@gmail.com>
2024-07-30 16:25:37 +0700

    fixed schema index

--- schemas/index.ts
+++ schemas/index.ts
@@ -39,7 +39,7 @@ export const SettingsSchema = z
     .object({
         name: z.optional(z.string()),
         isTwoFactorEnabled: z.optional(z.boolean()),
-        role: z.enum([UserRole.ADMIN, UserRole.USER]),
+        role: z.enum([UserRole.ADMIN]),
         email: z.optional(z.string().email()),
         password: z.optional(z.string().min(6)),
         newPassword: z.optional(z.string().min(6)),
