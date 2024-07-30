fb8ebf999b696e3210263b6626d945d1aa8d92e7
unghoangnam <unghoangnam389@gmail.com>
2024-07-30 16:13:28 +0700

    fixed email stuff

--- components/email.tsx
+++ components/email.tsx
@@ -1,34 +1,34 @@
-import { Html, Body, Section, Text } from "@react-email/components";
+import { Html, Body, Section, Text } from '@react-email/components'
 
 interface EmailProps {
-  name: string;
-  email?: string;
-  phone: string;
-  address: string;
-  data: any;
+    name: string
+    email?: string
+    phone: string
+    address: string
+    data: any
 }
 
 export function Email({ name, email, phone, address, data }: EmailProps) {
-  return (
-    <Html>
-      <Body>
-        <Section>
-          <Text>Name: {name}</Text>
-          <Text>Email: {email}</Text>
-          <Text>Phone: {phone}</Text>
-          <Text>Address: {address}</Text>
-        </Section>
-        <Section>
-          <Text>Please review the order details:</Text>
-          {data.map((item: any) => (
+    return (
+        <Html>
+            <Body>
+                <Section>
+                    <Text>Name: {name}</Text>
+                    <Text>Email: {email}</Text>
+                    <Text>Phone: {phone}</Text>
+                    <Text>Address: {address}</Text>
+                </Section>
+                <Section>
+                    <Text>Please review the order details:</Text>
+                    {/* {data.map((item: any) => (
             <Text id={item.uuid}>
               {item.name} ({Object.values(item.options).join(", ")}) x
               {item.quantity}
             </Text>
-          ))}
-        </Section>
-        <Section></Section>
-      </Body>
-    </Html>
-  );
+          ))} */}
+                </Section>
+                <Section></Section>
+            </Body>
+        </Html>
+    )
 }
